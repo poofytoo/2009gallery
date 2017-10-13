eefe240952fdd320450852708f1851243d831b43
poofytoo <victorhung92@gmail.com>
2017-10-12 20:27:59 -0400

    added gallery view page sidebar

--- js/view.js
+++ js/view.js
@@ -1,3 +1,12 @@
+var sections = {
+    'team' : 1,
+    '3-ideas' : 2,
+    'sketch-models' : 3,
+    'mock-ups' : 4
+}
+
+var scrollBreaks = [];
+
 function updateNavigationBar() {
     if ($(window).scrollTop() < 10) {
         $('.navigation').removeClass('condensed');
@@ -10,12 +19,54 @@ function updateNavigationBar() {
     }
 }
 
-$(function() {
-    console.log('s')
+function updateSidemenuHighlight() {
+    var closestSection = 0;
+    var section = 'team';
+    var scrollTop = $(window).scrollTop();
+    var offset = 200
+    for (i in sections) {
+        if (sections[i] < scrollTop + offset && closestSection < sections[i]) {
+            closestSection = sections[i] ;
+            section = i;
+        }
+    }
+    $('h3').each(function() {
+        $('li').removeClass('sidemenu-highlight')
+    })
+    $('li.m-' + section).addClass('sidemenu-highlight')
+}
 
-    $(window).scroll(function() {
+function scrollToSection(section) {
+    $(window).scrollTo($('h3.' + section), {
+        offset: -50,
+        duration: 200
+    })
+    updateSidemenuHighlight();
+}
+
+function buildSidemenu() {
+    $('h3').each(function() {
+        sections[$(this).attr('class')] = $(this).offset().top;
+    })
+    $('.project-sidemenu').on('click', 'li', function () {
+        scrollToSection($(this).data('section'));
+    })
+}
+
+$(function () {
+    $(window).scroll(function () {
+        updateSidemenuHighlight();
         updateNavigationBar();
     });
 
     updateNavigationBar();
+    buildSidemenu();
+
+    locationHash = window.location.hash;
+    if (locationHash) {
+        if ('scrollRestoration' in history) {
+            history.scrollRestoration = 'manual';
+        }
+        scrollToSection(locationHash.replace(/[^\-A-Za-z0-9]/g, ''));
+    }
 })
