32aa4c2ec591c40dde150a0a7ac8b85cb0b8ea93
Stephanie Yu <stephaniey@palantir.com>
2017-10-12 23:11:23 -0400

    Fix scrolling, remove team.jsx in favor of view.jsx

--- js/view.jsx
+++ js/view.jsx
@@ -117,6 +117,66 @@ var TeamContent = React.createClass({
     }
 });
 
+var sections = {
+    'team' : 1,
+    '3-ideas' : 2,
+    'sketch-models' : 3,
+    'mock-ups' : 4
+}
+
+var scrollBreaks = [];
+
+function updateNavigationBar() {
+    if ($(window).scrollTop() < 10) {
+        $('.navigation').removeClass('condensed');
+        $('h1').removeClass('condensed');
+        $('h2').removeClass('condensed');
+    } else {
+        $('.navigation').addClass('condensed');
+        $('h1').addClass('condensed');
+        $('h2').addClass('condensed');
+    }
+}
+
+function updateSidemenuHighlight() {
+    var closestSection = 0;
+    var section = 'team';
+    var scrollTop = $(window).scrollTop();
+    var offset = 200
+    for (var i in sections) {
+        if (sections[i] < scrollTop + offset && closestSection < sections[i]) {
+            closestSection = sections[i] ;
+            section = i;
+        }
+    }
+    $('h3').each(function() {
+        $('li').removeClass('sidemenu-highlight');
+    })
+    $('li.m-' + section).addClass('sidemenu-highlight');
+
+    window.location.hash = section;
+
+}
+
+function scrollToSection(section) {
+    $(window).scrollTo($('h3.' + section), {
+        offset: -50,
+        duration: 200
+    })
+    updateSidemenuHighlight();
+}
+
+function buildSidemenu() {
+    $('h3').each(function() {
+        sections[$(this).attr('class')] = $(this).offset().top;
+    })
+    $('.project-sidemenu').on('click', 'li', function () {
+        scrollToSection($(this).data('section'));
+    })
+}
+
+var locationHash;
+
 $(function () {
     function parseQuery(qstr) {
         if (qstr === undefined || qstr.length === 0) {
@@ -146,6 +206,26 @@ $(function () {
                 <TeamContent project={teamProject} year={urlLocation.year} />,
                 document.getElementById('project-content')
             );
+
+            new Clipboard('.btn');
+
+            // handle scrolling after the DOM has rendered our elements
+            $(window).scroll(function () {
+                updateSidemenuHighlight();
+                updateNavigationBar();
+            });
+
+            updateNavigationBar();
+            buildSidemenu();
+
+            locationHash = window.location.hash;
+            if (locationHash !== undefined) {
+                if ('scrollRestoration' in history) {
+                    history.scrollRestoration = 'manual';
+                }
+                console.log(locationHash);
+                scrollToSection(locationHash.replace(/[^\-A-Za-z0-9]/g, ''));
+            }
         } else {
             // handle unknown case
         }
