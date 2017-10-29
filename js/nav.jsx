4994ec18dec7b8593c05eefc09de53b52978c333
poofytoo <victorhung92@gmail.com>
2017-10-29 16:55:34 -0400

    navigation team list

--- js/nav.jsx
+++ js/nav.jsx
@@ -4,17 +4,42 @@ export const Navigation = React.createClass({
     render: function () {
         return <div>hello</div>;
     },
+    renderProductList: function () {
+        var projectList = [];
+        var year = 2015; // dynamically loaded
+        var projects = DATA[year].projects
+        for (var i in projects) {
+            var backgroundImage = `background-image: url('${baseUrl}${year}/final/photos/small/${i}1.jpg')`;
+            projectList.push(
+                <li class="selected-dropdown-item">
+                    <a href="">
+                        <div class="product-image" style={backgroundImage}></div>
+                        <div class="product-text">
+                            <em>{projects[i].projName}</em> {i} Team
+                        </div>
+                    </a>
+                </li>
+            )
+        }
+        return <div>{projectList}</div>
+    }
 });
 
 // dropdown-selector-list
 
+
 /*
 <li>
-<a href="">
-  <div class="product-image" style="background-image: url('http://designed.mit.edu/gallery/data/2015/final/photos/small/red1.jpg')"></div>
-  <div class="product-text">
-    <em>Trellis</em> Red Team
-  </div>
-</a>
-</li>
-*/
+              <a class="select-none" href="">
+                <span class="dim">View All Products</span>
+              </a>
+            </li>
+            <li class="selected-dropdown-item">
+              <a href="">
+                <div class="product-image" style="background-image: url('http://designed.mit.edu/gallery/data/2015/final/photos/small/blue1.jpg')"></div>
+                <div class="product-text">
+                  <em>Laser Kites</em> Blue Team
+                </div>
+              </a>
+            </li>
+            */
